dbf8fbe74ad10d92dad8dc8509272da01ff8cfbd
Lilliann02 <149729331+Lilliann02@users.noreply.github.com>
2023-11-03 11:08:05 +0200

    n

--- ДЗ.2/ДЗ.2-4.py
+++ ДЗ.2/ДЗ.2-4.py
@@ -1,3 +1,12 @@
 A = int(input("Введіть число A:  "))
 B = int(input("Введіть число B:  "))
 C = int(input("Введіть число C:  "))
+
+if (A<B and A>C) or (A>B and A<C):
+    print(A)
+elif (B<A and B>C) or (B>A and B<C):
+    print(B)
+elif (C<A and C>B) or (C>A and C<B):
+    print(C)
+else:
+    print("такого числа немає")
