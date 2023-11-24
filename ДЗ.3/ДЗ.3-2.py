745668b86df8ca9ac54ea391d4564f6d0911bb23
Lilliann02 <149729331+Lilliann02@users.noreply.github.com>
2023-11-24 03:20:35 +0200

    3-2

--- ДЗ.3/ДЗ.3-2.py
+++ ДЗ.3/ДЗ.3-2.py
@@ -1,4 +1,11 @@
-f=0
-while f>=0:
+X=0
+while not ((X//10)!=0 and (X//100)==0):
     X = int(input("Введіть двозначне число "))
-    f=X|100
+p10=X//10
+p1=X%10
+if p1>p10:
+    print(p1,">",p10)
+elif p10>p1:
+    print(p10,">",p1)
+else:
+    print(p1,"=",p10)
